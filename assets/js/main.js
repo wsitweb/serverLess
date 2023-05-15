d3ff91cc736d161bcf91a496961e681a89982118
ROMAN <96077350+wsitweb@users.noreply.github.com>
2023-05-15 05:08:51 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+const DOMelementsCodeTextLi = document.querySelectorAll('.code-text__list>li');
+function enumeration(elements, actions){
+    if(actions == 'data-num'){
+        let num = 0;
+        for(let elem of elements){
+            num++;
+            elem.dataset.num = num;
+        }
+    }
+}
+enumeration(DOMelementsCodeTextLi, 'data-num');
+
+const DOMelementHeader = document.querySelector('header');
+let switchScroll = 0;
+document.addEventListener('scroll', ()=>{switchClassHeader(100)});
+function switchClassHeader(position){
+    console.log(position)
+    if(Math.round(window.scrollY) > position){
+        DOMelementHeader.classList.add('active')
+        switchScroll++;
+    }else if(Math.round(window.scrollY) < position || switchScroll){
+        DOMelementHeader.classList.remove('active')
+        switchScroll--;
+    }
+}
+
